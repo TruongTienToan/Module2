19498bb69623dccb32f981071efc388c5deadcd9
TruongTienToan <truongtientoan2308@gmail.com>
2022-11-08 14:54:01 +0700

    Khai báo và sử dụng biến

---
+++ src/main/java/org/codegym/KhaiBaoSuDungBien/DeclareAndVariables.java
@@ -0,0 +1,18 @@
+package org.codegym.KhaiBaoSuDungBien;
+
+public class DeclareAndVariables {
+    public static void main(String[] args) {
+        int i = 10; // khai báo biến i là kiểu int có giá trị là 10
+        float f = 20.5f; // khai báo bến f là kiểu float có giá trị laf 20.5
+        double d = 20.5; // khai báo biến d có kiểu là double có giá trị là 20.5
+        boolean b = true; // khai báo biến b có kiểu là boolean có giá trị là true
+        char c = 'a'; // khai báo biến c có kiểu là char có giá trị là kí tự a
+        String s = "Hà Nội"; // khai báo biến s có kiểu là String có gi trị là chuỗi Hà Nội
+        System.out.println("i =" + i);
+        System.out.println("f =" + f);
+        System.out.println("d =" + d);
+        System.out.println("b =" + b);
+        System.out.println("c =" + c);
+        System.out.println("s =" + s);
+    }
+}
